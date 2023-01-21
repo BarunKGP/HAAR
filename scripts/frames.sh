ec03915bf7754ce553ce4679e0bca8cac404bbd7
Das, Barun <bdas31@gatech.edu>
2023-01-21 17:49:31 -0500

    Update frames.sh

--- scripts/frames.sh
+++ scripts/frames.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-FILE_DIR=$1
+FILE_DIR=/coc/scratch/bdas31/EPIC-Kitchens_100/2g1n6qdydwa9u22shpxqzp0t8m/$1
 echo "file directory: ${FILE_DIR}"
-for f in $FILE_DIR/*.tar; 
-do 
-bn=basename $f .tar
-echo "basename: ${bn}"
-mkdir $bn
-echo "created directory ${bn}"
-tar -xfC $bn "$f"
-echo "extracted ${f}"
-done
+# for f in $FILE_DIR/*.tar; 
+# do 
+# bn=basename $f .tar
+# echo "basename: ${bn}"
+# mkdir $bn
+# echo "created directory ${bn}"
+# tar -xfC $bn "$f"
+# echo "extracted ${f}"
+# done
