954e02eed5ce544116b88812d8000c5ce15fa2b9
Das, Barun <bdas31@gatech.edu>
2023-03-31 01:07:03 -0400

    Update models.py

--- scripts/models.py
+++ scripts/models.py
@@ -16,7 +16,7 @@ class WordEmbeddings(nn.Module):
         self.device = get_device()
 
     def forward(self, text):
-        embeddings = torch.tensor(self.model.encode(text))
+        embeddings = self.model.encode(text)
         return torch.from_numpy(embeddings)
 
     
