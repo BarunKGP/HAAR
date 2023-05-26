c45b4ee4e80aa5dac444d1199e74947debc62e29
Das, Barun <bdas31@gatech.edu>
2023-05-26 14:49:33 -0400

    debugging

--- src/train.py
+++ src/train.py
@@ -17,24 +17,31 @@ def main(cfg: DictConfig):
     LOG.info("Config:\n" + OmegaConf.to_yaml(cfg))
     data_module = EpicActionRecognitionDataModule(cfg)
     LOG.debug("EpicActionRecognitionDataModule initialized")
-    system = EpicActionRecognitionModule(cfg, data_module)
-    LOG.debug("EpicActionRecognitionSystem initialized")
-    if not cfg.get("log_graph", True):
-        # MTRN can't be traced due to the model stochasticity so causes a JIT tracer
-        # error, we allow you to prevent the tracer from running to log the graph when
-        # the summary writer is created
-        try:
-            delattr(system, "example_input_array")
-        except AttributeError:
-            pass
-    ddp = cfg.trainer.get("ddp", False)
-    if ddp:
-        ddp_setup()
-    LOG.info("Starting training....")
-    system.training_loop(1, cfg.model.save_path)
-    if ddp:
-        destroy_process_group()
-    LOG.info("Training completed!")
+
+    # debug
+    loader = data_module.val_dataloader()
+    for item in loader:
+        print(item)
+        break
+
+    # system = EpicActionRecognitionModule(cfg, data_module)
+    # LOG.debug("EpicActionRecognitionSystem initialized")
+    # if not cfg.get("log_graph", True):
+    #     # MTRN can't be traced due to the model stochasticity so causes a JIT tracer
+    #     # error, we allow you to prevent the tracer from running to log the graph when
+    #     # the summary writer is created
+    #     try:
+    #         delattr(system, "example_input_array")
+    #     except AttributeError:
+    #         pass
+    # ddp = cfg.trainer.get("ddp", False)
+    # if ddp:
+    #     ddp_setup()
+    # # LOG.info("Starting training....")
+    # system.training_loop(1, cfg.model.save_path)
+    # if ddp:
+    #     destroy_process_group()
+    # LOG.info("Training completed!")
 
 
 if __name__ == "__main__":
