ca8e31aab1236be36c06ab6228b51bc6c7d213f8
Das, Barun <bdas31@gatech.edu>
2023-05-28 00:41:36 -0400

    updated pilot data with accurate end frames

--- src/scratch/pilot_data.py
+++ src/scratch/pilot_data.py
@@ -1,8 +1,14 @@
 import os
+from pathlib import Path
 import pickle
+import re
+from natsort import natsorted
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from constants import PICKLE_ROOT
+
+from utils import write_pickle
 
 try:
     from constants import DATA_ROOT
@@ -18,13 +24,15 @@ except ImportError or ModuleNotFoundError:
 def _format_ds_(data_df, video_info_df):
     df = data_df.sort_values(by=["video_id", "narration_timestamp"])
     df = pd.merge(df, video_info_df, how="left", on="video_id")
+    final_frames = find_final_frames()
     df["start_frame"] = df.apply(
         lambda row: int(get_sec(row["narration_timestamp"]) * row["fps"]) + 1,
         axis=1,
     )
     df["end_frame"] = df.groupby("video_id")["start_frame"].shift(-1, fill_value=0)
     df["end_frame"] = df.apply(
-        lambda row: int(row["duration"] * row["fps"]) + 1
+        # lambda row: int(row["duration"] * row["fps"]) + 1
+        lambda row: final_frames[row["video_id"]] + 1
         if row["end_frame"] == 0
         else row["end_frame"],
         axis=1,
@@ -91,6 +99,27 @@ def main():
     with open("data/test100_mod.pkl", "xb") as handle:
         pickle.dump(test_modified, handle)
     print("Wrote test pickle")
+
+
+def find_final_frames(root=PICKLE_ROOT, save_loc="../../data", write_results=True):
+    regex = re.compile(r"\d+")
+    final_frames = {}
+    root = Path(root)
+    for subdir in root.iterdir():
+        if subdir.is_dir():
+            subroot = Path(subdir) / "flow_frames"
+            for video_folder in subroot.iterdir():
+                if video_folder.is_dir():
+                    frames = (Path(video_folder) / "u").glob("*.jpg")
+                    frames = natsorted(frames, key=str, reverse=True)
+                    final_frame = [int(x) for x in regex.findall(frames[-1])]
+                    final_frames[os.path.basename(video_folder)] = final_frame
+
+    if write_results:
+        write_pickle(final_frames, save_loc)
+        print("Finished writing final_frames")
+
+    return final_frames
 
 
 if __name__ == "__main__":
