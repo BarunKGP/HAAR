051ae1d5f5fb6d3d94e79fe63f07ec3cf1b73982
Das, Barun <bdas31@gatech.edu>
2023-06-15 12:22:05 -0400

    refactoring

--- src/train.py
+++ src/train.py
@@ -18,7 +18,6 @@ LOG = get_loggers(name=__name__, filename="data/pilot-01/logs/train.log")
 def main(cfg: DictConfig):
     LOG.info("Config:\n" + OmegaConf.to_yaml(cfg))
     data_module = EpicActionRecognitionDataModule(cfg)
-    LOG.debug("EpicActionRecognitionDataModule initialized")
 
     # debug
     # loader = data_module.val_dataloader()
@@ -31,7 +30,6 @@ def main(cfg: DictConfig):
     #     break
 
     system = EpicActionRecognitionModule(cfg, data_module)
-    LOG.debug("EpicActionRecognitionSystem initialized")
     if not cfg.get("log_graph", True):
         # MTRN can't be traced due to the model stochasticity so causes a JIT tracer
         # error, we allow you to prevent the tracer from running to log the graph when
@@ -43,7 +41,6 @@ def main(cfg: DictConfig):
     ddp = cfg.trainer.get("ddp", False)
     if ddp:
         ddp_setup()
-    # LOG.info("Starting training....")
     system.run_training_loop(1, Path(cfg.model.save_path))
     if ddp:
         destroy_process_group()
